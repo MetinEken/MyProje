4f48cab0142e4922b864d825e9260563299246f1
MetinEken <77900261+MetinEken@users.noreply.github.com>
2022-06-06 21:14:48 +0200

    Update Proje01.java
    github dan yeni deisiklikler

--- src/myProje1/Proje01.java
+++ src/myProje1/Proje01.java
@@ -4,7 +4,10 @@ public class Proje01 {
 
 	public static void main(String[] args) {
 		System.out.println("git githup deneme");
-
+		System.out.println("git githup deneme");
+		System.out.println("git githup deneme");
+		System.out.println("git githup deneme");
+		System.out.println("git githup deneme"); // github daki degisiklikler
 	}
 
 }
