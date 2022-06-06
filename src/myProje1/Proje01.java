1b7c50b932f039f4be2deb2399542c0e2668fef5
MetinEken <metineken2021@gmail.com>
2022-06-06 21:30:43 +0200

    06.06

--- src/myProje1/Proje01.java
+++ src/myProje1/Proje01.java
@@ -8,6 +8,8 @@ public class Proje01 {
 		System.out.println("git githup deneme");
 		System.out.println("git githup deneme");
 		System.out.println("git githup deneme"); // github daki degisiklikler
+	System.out.println("yeni degisiklik");
+	System.out.println("bidaha degisiklik");
 	}
 
 }
